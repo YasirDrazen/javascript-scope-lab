23addf38034c3acfa051ab1ca76a70b147c84319
Yasir <yasirdrazen@gmail.com>
2026-04-17 00:33:15 +0300

    Add scope lab solution

--- index.js
+++ index.js
@@ -1,25 +1,19 @@
 // Step 1: Global variables
-let burgers = ["Hamburger", "Cheeseburger"];
+const burgers = ["Hamburger", "Cheeseburger"];
 let featuredDrink = "Strawberry Milkshake";
 
 // Step 2: Function
 function addBurger() {
-  let newBurger = "Flatburger";
+  const newBurger = "Flatburger";
   burgers.push(newBurger);
 }
 
 // Step 3: Block scope
 if (true) {
-  let anotherNewBurger = "Maple Bacon Burger";
+  const anotherNewBurger = "Maple Bacon Burger";
   burgers.push(anotherNewBurger);
-
-  function changeFeaturedDrink() {
-    featuredDrink = "The JavaShake";
-  }
 }
-//Test
-addBurger();
-changeFeaturedDrink();
 
-console.log(burgers);
-console.log(featuredDrink);    
+function changeFeaturedDrink() {
+  featuredDrink = "The JavaShake";
+} 
